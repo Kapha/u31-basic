b275002056c76ccb4f33dc604e442232fae98a6f
Peter Blomgren <blomgren-p@mssu.edu>
2012-10-09 15:54:40 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    firstname   "Peter"
+    sirname     "Blomgren"
+    email       "mr@pablomgren.com"
+    password    "myg3tjw"
+    password_confirmation "myg3tjw"
+  end
+end
